f883d38fe70ad5ea125922de81896b996b7482ae
antongrutsin <Risenka13524>
2021-06-15 00:33:29 +0300

    Add ProductService (will expand)

---
+++ src/main/java/ru/antongrutsin/ecommerce/service/ProductService.java
@@ -0,0 +1,35 @@
+package ru.antongrutsin.ecommerce.service;
+
+import javassist.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.antongrutsin.ecommerce.domain.Product;
+import ru.antongrutsin.ecommerce.repository.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private ProductRepository productRepository;
+
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
+
+
+    public Product getById(long id) throws NotFoundException {
+        Optional<Product> element = productRepository.findById(id);
+        return element.orElseThrow(() -> new NotFoundException("Entity not found or id is wrong"));
+    }
+
+    public Product save(Product entity) {
+        return productRepository.save(entity);
+    }
+
+    public void delete(long id) {
+        productRepository.deleteById(id);
+    }
+}
