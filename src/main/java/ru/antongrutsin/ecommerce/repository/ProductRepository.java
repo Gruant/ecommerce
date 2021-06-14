2370568bf0eba92e51a91f06be138d44f31a0976
antongrutsin <Risenka13524>
2021-06-15 00:27:28 +0300

    Add method for filter in ProductRepository

--- src/main/java/ru/antongrutsin/ecommerce/repository/ProductRepository.java
+++ src/main/java/ru/antongrutsin/ecommerce/repository/ProductRepository.java
@@ -1,6 +1,5 @@
 package ru.antongrutsin.ecommerce.repository;
 
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.antongrutsin.ecommerce.domain.Category;
 import ru.antongrutsin.ecommerce.domain.Product;
@@ -11,6 +10,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     /*
     Методы для фильтров на странице
      */
-    List<Product> findProductByCostGreaterThanEqualAndCostLessThanEqual(double cost, double cost2, Pageable pageable);
+    List<Product> findProductByCostGreaterThanEqualAndCostLessThanEqual(double cost, double cost2);
     List<Product> findProductByCategoryId(Category category);
+    List<Product> findProductByCategoryIdAndCostGreaterThanEqualAndCostLessThanEqual(Category category, double cost, double cost2);
 }
