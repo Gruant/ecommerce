833fd23cc076c577e8c16e29667e13b903c00a52
antongrutsin <Risenka13524>
2021-06-14 23:52:29 +0300

    Add ProductRepository

---
+++ src/main/java/ru/antongrutsin/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package ru.antongrutsin.ecommerce.repository;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.antongrutsin.ecommerce.domain.Category;
+import ru.antongrutsin.ecommerce.domain.Product;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    /*
+    Методы для фильтров на странице
+     */
+    List<Product> findProductByCostGreaterThanEqualAndCostLessThanEqual(double cost, double cost2, Pageable pageable);
+    List<Product> findProductByCategoryId(Category category);
+}
